98163cd8eb55dd82889bb918640c74e66e381d21
Fourfinger <113786447+Fourfinger92@users.noreply.github.com>
2025-01-13 21:50:55 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,14 +1,8 @@
 fx_version 'adamant'
 
---author '_stoned.' for sure not this guy, he leaked my code
-author 'Fourfinger'
 game 'gta5'
 
 lua54 'yes'
-
-shared_scripts {
-    'config.lua',
-}
 
 server_scripts {
     'server.lua',
